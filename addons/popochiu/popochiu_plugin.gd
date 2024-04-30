ed30383ee74f80c8ad017d46302b68ef95aec89b
Anthony Irwin <git@anthonyirwin.com>
2024-04-30 17:55:56 +1000

    add initial migration framework support

--- addons/popochiu/popochiu_plugin.gd
+++ addons/popochiu/popochiu_plugin.gd
@@ -53,6 +53,9 @@ func _enter_tree() -> void:
 	prints(EN)
 	print_rich("[wave]%s[/wave]" % SYMBOL)
 	
+	# Chech if new migrations exist and run them if they do
+	DoMigration.do_migrations()
+	
 	_editor_file_system.scan_sources()
 	
 	PopochiuEditorConfig.initialize_editor_settings()
